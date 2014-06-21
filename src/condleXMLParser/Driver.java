d6713e014d5fda1c6766e600c0544912ffe7605e
Mandeep Condle <mandeepcondle@gmail.com>
2014-06-21 16:28:19 -0400

    sample xml and driver

---
+++ src/condleXMLParser/Driver.java
@@ -0,0 +1,12 @@
+package condleXMLParser;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        XMLParser test = new XMLParser("~/Developer/IdeaProjects/XMLParser");
+
+
+    }
+
+}
